1311ead878a74576d66dedfc3edc5ef0294aea2d
moomu-owo <71679979+moomu-owo@users.noreply.github.com>
2020-12-15 16:00:42 +0800

    1215 npc程式

---
+++ Unity_asia_3Dgame/Assets/script/NPC.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NPC: MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCdata data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+
+    ///玩家是否進入感應區
+    public bool playerInArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "ming")
+        {
+            playerInArea = true;
+            Dialog();
+
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "ming")
+        {
+            playerInArea = false;
+        }
+    }
+    private void Dialog()
+    {
+        // print(data.conversationA);
+        // print(data.conversationA[5]);
+
+
+        //for 迴圈:重複處裡相同程式
+        for (int i = 0; i < data.conversationA.Length; i++)
+        {
+            print(data.conversationA[i]);
+        }
+    }
+}
---
+++ Unity_asia_3Dgame/Assets/script/NPCdata.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName ="NPC 資料" , menuName ="MU/NPC 資料")]
+public class NPCdata : ScriptableObject
+{
+    [Header("第一段對話") , TextArea(1,5)]
+    public string conversationA;
+    [Header("第二段對話"), TextArea(1, 5)]
+    public string conversationB;
+    [Header("第三段對話"), TextArea(1, 5)]
+    public string conversationC;
+
+    [Header("任務需求項目數量")]
+    public int count;
+
+    [Header("已經取得項目數量")]
+    public int countCurrent;
+
+}
